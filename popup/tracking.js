59e4f0f5625846883456331412dc4f6f3e98130b
c4rd3 <carlos.cardenal88@gmail.com>
2020-07-26 19:04:11 +0200

    First mock returning data to log.

--- popup/tracking.js
+++ popup/tracking.js
@@ -9,61 +9,80 @@ function onError(error) {
     console.log(error);
 }
 
-/* display previously-saved stored notes on startup */
+/* display previously-saved stored issues on startup */
 initialize();
 
 function initialize() {
-  var gettingAllStorageItems = browser.storage.local.get(null);
-  gettingAllStorageItems.then((results) => {
-    var issuesKeys = Object.keys(results);
-    for (let issueKey of issuesKeys) {
-      var status = results[issueKey];
-      displayIssue(issueKey,status);
-    }
-  }, onError);
-}
-
-
-function addIssue() {
+    /** Load local issues */
+    var gettingAllStorageItems = browser.storage.local.get(null);
     
-    /** Recover issue ID */
-    var issueID = inputIssue.value;
-    /** Guardar issue*/
-    var storingIssue = browser.storage.local.set({[issueID]:'prueba'});
-    
-    storingIssue.then(()=> {
-        displayIssue(issueID, 'prueba');    
+    /** Display every issue found */
+    gettingAllStorageItems.then((results) => {
+        var issuesKeys = Object.keys(results);
+        for (let issueKey of issuesKeys) {
+            var summary = results[issueKey]['summary'];
+            var status = results[issueKey]['status'];
+            displayIssue(issueKey, summary, status);
+        }
     }, onError);
 }
- 
 
- 
-function removeIssue(issue) {
+function addIssue() {  
+    /** Recover issue ID */
+    var issueID = inputIssue.value;
     
+    /** Save issue*/
+    let storeIssue = {}
+    storeIssue[issueID] = {}
+    storeIssue[issueID]['summary'] = 'ey que pasa';
+    storeIssue[issueID]['status'] = 'blocked';
+    var storingIssue = browser.storage.local.set(storeIssue);
+    
+    /** Show new issue */
+    storingIssue.then(()=> {
+        displayIssue(issueID, 'descrip', 'prueba');    
+    }, onError);
+    getIssue();
+}
+ 
+function getIssue() {
+    fetch('https://run.mocky.io/v3/6bd7b6bb-afb9-4770-9e4f-b577a2494ae9')
+    .then(response => response.json())
+    .then(function(data){
+        console.log(data);
+        console.log(data.fields['sub-tasks'][0]['outwardIssue']['fields']['status']['name']);
+        console.log(data.self);
+    });
 }
 
+function displayIssue(issue, summary, status) {
 
-function displayIssue(issue, status) {
-  var issueContainer = document.createElement('div');
-  var issueDisplay = document.createElement('div');
-  var issueStatus = document.createElement('h3');
-  var deleteBtn = document.createElement('button');
+    /** Crear DOM elements */
+    var issueContainer = document.createElement('div');
+    var issueID = document.createElement('div');
+    var issueSummary = document.createElement('div');
+    var issueStatus = document.createElement('div');
+    var deleteBtn = document.createElement('button');
 
-  issueDisplay.textContent = issue;
-  issueStatus.textContent = status;
-  deleteBtn.setAttribute('class', 'button delete');
-  deleteBtn.textContent = 'remove';
+    /** Fill elements with data */
+    issueID.textContent = issue;
+    issueSummary.textContent = summary;
+    issueStatus.textContent = status;
+    deleteBtn.setAttribute('class', 'button delete');
+    deleteBtn.textContent = 'remove';
 
-  issueContainer.appendChild(issueDisplay);
-  issueContainer.appendChild(issueStatus);
-  issueContainer.appendChild(deleteBtn);
+    /** Build issue node */
+    issueContainer.appendChild(issueID);
+    issueContainer.appendChild(issueSummary);
+    issueContainer.appendChild(issueStatus);
+    issueContainer.appendChild(deleteBtn);
       
-  issues.appendChild(issueContainer); 
+    issues.appendChild(issueContainer); 
 
-  deleteBtn.addEventListener('click',(e) => {
-    const evtTgt = e.target;
-    evtTgt.parentNode.parentNode.removeChild(evtTgt.parentNode);
-    browser.storage.local.remove(issue);
-  })
-
+    /** Bind delete on click event */
+    deleteBtn.addEventListener('click',(e) => {
+        const evtTgt = e.target;
+        evtTgt.parentNode.parentNode.removeChild(evtTgt.parentNode);
+        browser.storage.local.remove(issue);
+    })
 }
